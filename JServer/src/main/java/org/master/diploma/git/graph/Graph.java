a77bbd16b5a55f4b647af639c5af6f63bc6ed517
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2025-01-07 01:34:45 +0300

    [java] создан интерфейс класса Graph

--- JServer/src/main/java/org/master/diploma/git/graph/Graph.java
+++ JServer/src/main/java/org/master/diploma/git/graph/Graph.java
@@ -1,6 +1,16 @@
 package org.master.diploma.git.graph;
 
+import java.util.List;
+
 public abstract class Graph {
 
+    public abstract List<Vertex> getVertices();
+    public abstract Vertex getVertex(int number);
+    public abstract void removeVertex(int number);
 
+    public abstract List<Integer> getParentNumbers();
+    public abstract List<Vertex> getParents();
+
+    public abstract List<Integer> getChildrenNumbers();
+    public abstract List<Vertex> getChildren();
 }

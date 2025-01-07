ff9dd59f45328a06b939076409db115ad4c20dd6
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2025-01-07 17:42:03 +0300

    [java-core] создан класс исключение

---
+++ JServer/src/main/java/org/master/diploma/git/graph/exception/IncorrectVertexNumberException.java
@@ -0,0 +1,7 @@
+package org.master.diploma.git.graph.exception;
+
+public class IncorrectVertexNumberException extends RuntimeException {
+    public IncorrectVertexNumberException(String message) {
+        super(message);
+    }
+}

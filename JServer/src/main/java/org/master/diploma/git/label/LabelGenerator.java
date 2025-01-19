f344af19111b58fe043f1e726e03a6daea839e22
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2025-01-20 00:50:07 +0300

    [java] создан абстрактный класс LabelGenerator

---
+++ JServer/src/main/java/org/master/diploma/git/label/LabelGenerator.java
@@ -0,0 +1,8 @@
+package org.master.diploma.git.label;
+
+import org.master.diploma.git.git.model.CommitGraph;
+
+public abstract class LabelGenerator {
+
+    public abstract void makeLabelFprGitGraph(CommitGraph commitGraph);
+}

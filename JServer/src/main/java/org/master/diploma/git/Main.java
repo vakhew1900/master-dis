5a7ecf6930e7b0509c812aa471f8e9e7ce56fc92
Антон Чупинин <chupinin.a@volgablob.ru>
2025-05-22 10:51:34 +0300

    [brute-force] добавлен класс PermutationHelper

--- JServer/src/main/java/org/master/diploma/git/Main.java
+++ JServer/src/main/java/org/master/diploma/git/Main.java
@@ -2,37 +2,43 @@ package org.master.diploma.git;
 
 import org.master.diploma.git.git.GitHelper;
 import org.master.diploma.git.git.model.Commit;
+import org.master.diploma.git.support.PermutationHelper;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String path = "E:\\univer\\5_course\\diploma\\CAP\\soft\\master-dis\\JServer\\src\\test\\resources\\repositories\\test-1";
-        var gitGraph = GitHelper.createCommitGraph(path);
-        gitGraph.getVertices().forEach(
-                vertex -> {
-                    ((Commit) vertex).getDiffs().forEach(
-                            System.out::println
-                    );
-                }
-        );
+//        String path = "E:\\univer\\5_course\\diploma\\CAP\\soft\\master-dis\\JServer\\src\\test\\resources\\repositories\\test-1";
+//        var gitGraph = GitHelper.createCommitGraph(path);
+//        gitGraph.getVertices().forEach(
+//                vertex -> {
+//                    ((Commit) vertex).getDiffs().forEach(
+//                            System.out::println
+//                    );
+//                }
+//        );
+//
+//        gitGraph.getVertices().forEach(
+//                vertex -> {
+//                    ((Commit) vertex).getDiffEntries().forEach(
+//                            diffEntry -> {
+//                             System.out.println("old" + diffEntry.getOldPath());
+//                             System.out.println("new" + diffEntry.getNewPath());
+//                            }
+//                    );
+//                }
+//        );
+//
+//        System.out.println("Ffff");
 
-        gitGraph.getVertices().forEach(
-                vertex -> {
-                    ((Commit) vertex).getDiffEntries().forEach(
-                            diffEntry -> {
-                             System.out.println("old" + diffEntry.getOldPath());
-                             System.out.println("new" + diffEntry.getNewPath());
-                            }
-                    );
-                }
-        );
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
 
-        System.out.println("Ffff");
+         //   System.out.println(PermutationHelper.generatePermutations(n, k));
+        }
     }
 }
---
+++ JServer/src/main/java/org/master/diploma/git/graph/BruteForceMethod.java
@@ -0,0 +1,4 @@
+package org.master.diploma.git.graph;
+
+public class BruteForceMethod {
+}

d7b6878364d5693f1060c60f0e33c3f1c6c3f01f
Антон Чупинин <chupinin.a@volgablob.ru>
2025-05-22 12:29:27 +0300

    [test] создан класс GraphResult

--- JServer/src/main/java/org/master/diploma/git/graph/BruteForceMethod.java
+++ JServer/src/main/java/org/master/diploma/git/graph/BruteForceMethod.java
@@ -1,4 +1,28 @@
 package org.master.diploma.git.graph;
 
 public class BruteForceMethod {
+
+
+    public static <T extends Vertex> void execute(
+            Graph<T> first,
+            Graph<T> second
+    ) {
+        boolean invert = first.getVertices().size() < second.getVertices().size();
+
+        if (invert) {
+            var tmp = first;
+            first = second;
+            second = tmp;
+        }
+
+
+        Map<Integer, Integer>
+        for (T u : first.getVertices()) {
+            for (T v : second.getVertices()) {
+
+                if (u)
+            }
+        }
+    }
+
 }
---
+++ JServer/src/main/java/org/master/diploma/git/graph/GraphResult.java
@@ -0,0 +1,27 @@
+package org.master.diploma.git.graph;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GraphResult {
+
+    boolean invert = false;
+    private Map<Integer, Integer> matchingVertices = new HashMap<>();
+
+    private Map<Integer, LabelError> labelErrors;
+
+    public static class LabelError {
+        public static final String EXTRA = "extra_labels";
+        public static final String MISSING = "missing_labels";
+
+        @SerializedName(EXTRA)
+        private List<Integer> extraLabels;
+
+        @SerializedName(MISSING)
+        private List<Integer> missingLabels;
+    }
+
+}

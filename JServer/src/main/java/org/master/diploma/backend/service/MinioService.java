050993956bf978b61f04f2b93a138c881f3d9722
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2026-05-10 23:34:03 +0300

    [backend] обновлен MinioService

--- JServer/src/main/java/org/master/diploma/backend/service/MinioService.java
+++ JServer/src/main/java/org/master/diploma/backend/service/MinioService.java
@@ -1,18 +1,43 @@
 package org.master.diploma.backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
+@RequiredArgsConstructor
 public class MinioService {
+    private final ZipProcessingService zipProcessingService;
+
     public String uploadFile(String bucketName, String objectName, InputStream inputStream, long size, String contentType) {
-        // TODO: Real MinIO implementation
+        // TODO: Real MinIO implementation (e.g., using MinioClient)
         return "minio://" + bucketName + "/" + objectName;
     }
 
     public InputStream downloadFile(String bucketName, String objectName) {
         // TODO: Real MinIO implementation
         return null;
+    }
+
+    /**
+     * Downloads a repository (as a ZIP) from MinIO and unzips it to a temporary directory.
+     * @param repoPath Path in MinIO (e.g., minio://bucket/object)
+     * @return File object pointing to the unzipped repository directory.
+     */
+    public File downloadRepository(String repoPath) throws IOException {
+        // 1. Create a temporary directory
+        Path tempDir = Files.createTempDirectory("jserver_repo_");
+        
+        // 2. Get InputStream from MinIO
+        // String objectName = repoPath.substring(repoPath.lastIndexOf("/") + 1);
+        // InputStream is = downloadFile("repositories", objectName);
+        
+        // FOR STUB: Assume we have some way to get the ZIP.
+        // In real implementation:
+        // zipProcessingService.unzip(is, tempDir);
+        
+        return tempDir.toFile();
     }
 }

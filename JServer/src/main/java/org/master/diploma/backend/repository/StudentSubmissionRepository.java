eec1caf0fda479dacedf4a2bcd6e40079fcbdadc
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2026-05-10 22:37:20 +0300

    [backend] добавлены репозитории

---
+++ JServer/src/main/java/org/master/diploma/backend/repository/StudentSubmissionRepository.java
@@ -0,0 +1,11 @@
+package org.master.diploma.backend.repository;
+
+import org.master.diploma.backend.entity.StudentSubmission;
+import org.master.diploma.backend.entity.Task;
+import org.master.diploma.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface StudentSubmissionRepository extends JpaRepository<StudentSubmission, Long> {
+    Optional<StudentSubmission> findByStudentAndTask(User student, Task task);
+}

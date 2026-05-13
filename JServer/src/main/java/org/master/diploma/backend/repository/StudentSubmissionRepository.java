a5d64eafb446423d05974d7b0872d291f3e2d94d
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2026-05-14 01:43:37 +0300

    [backend] небольшой рефакторинг

--- JServer/src/main/java/org/master/diploma/backend/repository/StudentSubmissionRepository.java
+++ JServer/src/main/java/org/master/diploma/backend/repository/StudentSubmissionRepository.java
@@ -4,8 +4,10 @@ import org.master.diploma.backend.entity.StudentSubmission;
 import org.master.diploma.backend.entity.Task;
 import org.master.diploma.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentSubmissionRepository extends JpaRepository<StudentSubmission, Long> {
     Optional<StudentSubmission> findByStudentAndTask(User student, Task task);
+    List<StudentSubmission> findByStudent(User student);
 }

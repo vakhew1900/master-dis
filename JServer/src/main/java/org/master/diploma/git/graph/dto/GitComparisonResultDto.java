d6873b35bf158e3c919523ef40f04a0bb166fed1
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2026-05-14 01:00:59 +0300

    [backend] мелкие доработки api

--- JServer/src/main/java/org/master/diploma/git/graph/dto/GitComparisonResultDto.java
+++ JServer/src/main/java/org/master/diploma/git/graph/dto/GitComparisonResultDto.java
@@ -1,10 +1,29 @@
 package org.master.diploma.git.graph.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.master.diploma.git.graph.dto.merged_graph.MergedGraphComparisonResultDto;
+import org.master.diploma.git.graph.dto.two_graph.TwoGraphComparisonResultDto;
+
 /**
  * Base interface for Git comparison result Data Transfer Objects.
  * This allows different visualization strategies (Two-Graph vs Merged-Graph) 
  * to be handled by a unified reporting system.
  */
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = TwoGraphComparisonResultDto.class, name = "two_graph"),
+    @JsonSubTypes.Type(value = MergedGraphComparisonResultDto.class, name = "merged_graph")
+})
+@Schema(
+    oneOf = {TwoGraphComparisonResultDto.class, MergedGraphComparisonResultDto.class},
+    discriminatorProperty = "type"
+)
 public interface GitComparisonResultDto {
     // Marker interface for comparison result DTOs
 }

6266f0dfcbebc1ac7dfc4e449ef8053bebb79655
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2026-05-27 15:10:21 +0300

    [fix] добавлены AdminTaskService

---
+++ JServer/src/main/java/org/master/diploma/backend/service/AdminTaskService.java
@@ -0,0 +1,37 @@
+package org.master.diploma.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.master.diploma.backend.dto.admin.AdminTaskDto;
+import org.master.diploma.backend.entity.Task;
+import org.master.diploma.backend.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AdminTaskService {
+    private final TaskRepository taskRepository;
+
+    public List<AdminTaskDto> getAllTasks() {
+        return taskRepository.findAll().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public AdminTaskDto getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .map(this::convertToDto)
+                .orElse(null);
+    }
+
+    public AdminTaskDto convertToDto(Task task) {
+        return AdminTaskDto.builder()
+                .id(task.getId())
+                .number(task.getNumber())
+                .description(task.getDescription())
+                .referenceRepoPath(task.getReferenceRepoPath())
+                .build();
+    }
+}
